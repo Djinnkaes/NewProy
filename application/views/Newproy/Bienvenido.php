4636e1aae3e86520e2ebe8334716f73465b344cd
Kevin Espinoza <kaeskevin@gmail.com>
2017-05-16 05:39:49 -0400

    ultimos cambios en Bienvenido

--- application/views/Newproy/Bienvenido.php
+++ application/views/Newproy/Bienvenido.php
@@ -1,12 +1,22 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<!-- Latest compiled and minified CSS -->
+	<script src="//js/jquery-1.11.2.min.js"></script>
+	<script src="//code,jquery.com/jquery-1.11.2.min.js"></script>
+
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+	
 	<title>Bienvenido</title>
 	<meta charset='utf-8'>
 </head>
 <body>
 
-<p>LLamado desde bla bla byo que se dkasjhflkjashdfljahdla</p>
+<p>LLamado desde bla bla byo que se </p>
 
 </body>
 </html>
